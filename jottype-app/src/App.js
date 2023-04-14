d7732507eb9c94434d0149183d38cdd6ae0e9139
noahwx <noahfontenette@outlook.com>
2023-04-13 20:04:30 -0500

    4.0.000

--- jottype-app/src/App.js
+++ jottype-app/src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Note from './components/Note';
 import logo from './logo.svg';
 import Favicon from 'react-favicon';
 import './index.css';
 
-//Version 3.0.100
+//Version 4.0.000
 
 function App() {
 
+  const [theme, setTheme] = useState('light');
+
+    const toggleTheme = () => {
+      if (theme === 'light') {
+          setTheme('dark');
+      } else {
+          setTheme('light');
+      }
+  };
+
+  useEffect(() => {
+      localStorage.setItem('theme', theme);
+      document.body.className = theme;
+  }, [theme]);
+
   return (
     <>
-      <Favicon url={logo} />
-      <Note />
+      <div className={`App ${theme}`}>
+        <button onClick={toggleTheme} className='Themebtn'>Toggle Theme</button>
+        <Favicon url={logo} />
+        <Note toggleTheme={toggleTheme}/>
+      </div>
     </>
   );
 }
